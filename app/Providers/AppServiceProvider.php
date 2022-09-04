dde5afe3a68a818f88d17fbb5275003bb8ae1965
Mugiarto <minionxapp@gmail.com>
2022-09-04 21:29:00 +0700

    url migrate and seed

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-// use Illuminate\Support\Facades\URL; //unutk deploy https
+use Illuminate\Support\Facades\URL; //unutk deploy https
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -25,6 +25,6 @@ class AppServiceProvider extends ServiceProvider
     {
         //
         // AppServiceProvider
-        // URL::forceScheme('https');// untuk deploy https
+        URL::forceScheme('https');// untuk deploy https
     }
 }
